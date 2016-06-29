5965fbb77618b512d3e322d8910917ceae8f16fe
rajatvijay <rajatvijay5@gmail.com>
2016-06-29 06:42:14 -0700

    Added the django-pygments app
    This is guided in the github of django-pyments to install this app in the django project

--- pastebin/settings.py
+++ pastebin/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_pygments',
+    'general',
 ]
 
 MIDDLEWARE_CLASSES = [
