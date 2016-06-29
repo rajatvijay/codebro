3c293f662e2e0ba898649454dfd875a5b2bbd623
rajatvijay <rajatvijay5@gmail.com>
2016-06-29 05:39:17 -0700

    Created post_create view with form handling

--- general/views.py
+++ general/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .forms import PasteForm
 
-# Create your views here.
+import uuid
+
+
+def paste_create(request):
+    if request.method == 'POST':
+        paste_form = PasteForm(data=request.POST)
+
+        if paste_form.is_valid():
+            new_paste = paste_form.save(commit=False)
+            new_paste.slug = uuid.uuid4().hex[:6].upper()
+            new_paste.save()
+
+    else:
+        paste_form = PasteForm()
+
+    return render(request, 'general/paste/paste_form.html', {'form': paste_form})
+
