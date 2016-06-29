8a03033529cafef150fdaa82a62ff2db49fe53c0
rajatvijay <rajatvijay5@gmail.com>
2016-06-29 05:17:53 -0700

    Created the Paste model

--- general/models.py
+++ general/models.py
@@ -2,4 +2,18 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Paste(models.Model):
+
+    SYNTAX_HIGHLIGHTING = (('java', 'JAVA'),
+                           ('ruby', 'RUBY'),
+                           ('python', 'PYTHON'),
+                           ('html', 'HTML'),
+                           ('c', 'C'))
+
+    title = models.CharField(max_length=200)
+    syntax = models.CharField(choices=SYNTAX_HIGHLIGHTING)
+    paste = models.TextField()
+    slug = models.SlugField(max_length=6)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
